6110cdde4241362ae2ec2050c1f8993fa2fbc46f
jorgevs_GitHub <jorgevs.mx@gmail.com>
2019-02-04 01:08:09 -0500

    Updating deployment process

--- config-server/src/deploy/deploy.sh
+++ config-server/src/deploy/deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#$1 configUserName
+#$2 configPassword
+#$3 configURI
+#$4 configProfile
+#$5 configEncKey
+
 APP_BASE_DIR=/data/microservices/config-server
 DEPLOYMENT_FILE=config-server.jar
 STARTUP_SCRIPT=startup.sh
@@ -8,6 +14,8 @@ DEPLOYMENT_FOLDER=/home/jorgevs/deploy/
 # shutdown the application first
 #curl -X POST http://localhost:8091/manage/shutdown -H "authorization: Basic $1" -H 'cache-control: no-cache'
 curl -X POST 'http://localhost:8001/actuator/shutdown'
+
+echo 'params: ' $1 $2 $3 $4 $5
 
 # wait 15 seconds
 echo 'sleep 15 seconds'
@@ -18,3 +26,4 @@ cp ${DEPLOYMENT_FOLDER}/${DEPLOYMENT_FILE} ${APP_BASE_DIR}/bin
 cp ${DEPLOYMENT_FOLDER}/${STARTUP_SCRIPT} ${APP_BASE_DIR}/bin
 
 chmod +x ${APP_BASE_DIR}/bin/${STARTUP_SCRIPT}
+${APP_BASE_DIR}/bin/${STARTUP_SCRIPT} $1 $2 $3 $4 $5
--- config-server/src/deploy/startup.sh
+++ config-server/src/deploy/startup.sh
@@ -10,7 +10,5 @@ APP_BASE_DIR=/data/microservices/config-server
 DEPLOYMENT_FILE=config-server.jar
 JAVA_HOME=/data/java/
 
-echo 'params: ' $1 $2 $3 $4 $5
-
 #nohup $JAVA_HOME/bin/java -Xmx256m -Xss256k -DpropKey=$1 -Dprofile=$2 -DconfigUser=$3 -DconfigPass=$4 -DconfigURI="http://$5:8091/" -DserverUser=$6 -DserverPassword=$7 -jar ${APP_BASE_DIR}/bin/${DEPLOYMENT_FILE} > ${APP_BASE_DIR}/logs/config-server.log 2>&1 &
 nohup $JAVA_HOME/bin/java -Xmx256m -Xss256k -configUserName=$1 -configPassword=$2 -configURI=$3 -configProfile=$4 -configEncKey=$5 -jar ${APP_BASE_DIR}/bin/${DEPLOYMENT_FILE} > ${APP_BASE_DIR}/logs/config-server.log 2>&1 &
