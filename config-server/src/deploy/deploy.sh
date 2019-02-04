a492dcae30799866f70c8348e2ad7e1c1f19ad80
jorgevs_GitHub <jorgevs.mx@gmail.com>
2019-02-04 01:05:44 -0500

    Updating deployment process

--- config-server/src/deploy/deploy.sh
+++ config-server/src/deploy/deploy.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#$1 configUserName
-#$2 configPassword
-#$3 configURI
-#$4 configProfile
-#$5 configEncKey
-
 APP_BASE_DIR=/data/microservices/config-server
 DEPLOYMENT_FILE=config-server.jar
 STARTUP_SCRIPT=startup.sh
@@ -14,8 +8,6 @@ DEPLOYMENT_FOLDER=/home/jorgevs/deploy/
 # shutdown the application first
 #curl -X POST http://localhost:8091/manage/shutdown -H "authorization: Basic $1" -H 'cache-control: no-cache'
 curl -X POST 'http://localhost:8001/actuator/shutdown'
-
-echo 'params: ' $1 $2 $3 $4 $5
 
 # wait 15 seconds
 echo 'sleep 15 seconds'
