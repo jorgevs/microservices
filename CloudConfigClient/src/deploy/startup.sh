b4bb3c1e14f6589ac0faa98e7e583424bb00ce4c
jorgevs.mx <jorgevs.mx@gmail.com>
2019-07-27 22:06:55 -0400

    Fixing java URL

--- CloudConfigClient/src/deploy/startup.sh
+++ CloudConfigClient/src/deploy/startup.sh
@@ -9,6 +9,6 @@
 APP_NAME=config-client
 APP_BASE_DIR=/data/microREST/${APP_NAME}
 DEPLOYMENT_FILE=${APP_NAME}.jar
-JAVA_HOME=/data/java/
+JAVA_HOME=/data/java
 
 nohup $JAVA_HOME/bin/java -Xmx256m -Xss256k -DconfigUser=$1 -DconfigPass=$2 -DconfigURI=$3 -Dprofile=$4 -DpropKey=$5 -Dapp.home=${APP_BASE_DIR} -jar ${APP_BASE_DIR}/bin/${DEPLOYMENT_FILE} > ${APP_BASE_DIR}/logs/${APP_NAME}.log 2>&1 &
