af4a61dcd2350a03a9e2163f370525457bbbdb18
jorgevs.mx <jorgevs.mx@gmail.com>
2019-07-28 15:42:31 -0400

    Improving JVM settings

--- CloudConfigClient/src/deploy/startup.sh
+++ CloudConfigClient/src/deploy/startup.sh
@@ -11,4 +11,4 @@ APP_BASE_DIR=/data/microREST/${APP_NAME}
 DEPLOYMENT_FILE=${APP_NAME}.jar
 JAVA_HOME=/data/java
 
-nohup $JAVA_HOME/bin/java -Xmx256m -Xss256k -DconfigUser=$1 -DconfigPass=$2 -DconfigURI=$3 -Dprofile=$4 -DpropKey=$5 -Dapp.home=${APP_BASE_DIR} -jar ${APP_BASE_DIR}/bin/${DEPLOYMENT_FILE} > ${APP_BASE_DIR}/logs/${APP_NAME}.log 2>&1 &
+nohup $JAVA_HOME/bin/java -Xms128m -Xmx128m -Xss256k -DconfigUser=$1 -DconfigPass=$2 -DconfigURI=$3 -Dprofile=$4 -DpropKey=$5 -Dapp.home=${APP_BASE_DIR} -jar ${APP_BASE_DIR}/bin/${DEPLOYMENT_FILE} > ${APP_BASE_DIR}/logs/${APP_NAME}.log 2>&1 &
--- config-server/src/deploy/startup.sh
+++ config-server/src/deploy/startup.sh
@@ -11,4 +11,4 @@ APP_BASE_DIR=/data/microREST/${APP_NAME}
 DEPLOYMENT_FILE=${APP_NAME}.jar
 JAVA_HOME=/data/java
 
-nohup $JAVA_HOME/bin/java -Xmx256m -Xss256k -DgithubUser=$1 -DgithubPass=$2 -DconfigUser=$3 -DconfigPass=$4 -DpropKey=$5 -Dapp.home=${APP_BASE_DIR} -jar ${APP_BASE_DIR}/bin/${DEPLOYMENT_FILE} > ${APP_BASE_DIR}/logs/${APP_NAME}.log 2>&1 &
+nohup $JAVA_HOME/bin/java -Xms128m -Xmx128m -Xss256k -DgithubUser=$1 -DgithubPass=$2 -DconfigUser=$3 -DconfigPass=$4 -DpropKey=$5 -Dapp.home=${APP_BASE_DIR} -jar ${APP_BASE_DIR}/bin/${DEPLOYMENT_FILE} > ${APP_BASE_DIR}/logs/${APP_NAME}.log 2>&1 &
